23bdbc276b8418ead6994204f92543406dc563c3
liuxuedata <liuxuedata@gmail.com>
2025-09-07 22:09:51 +0800

    style: shrink KPI cards for small screens

--- api/test-site-configs.js
+++ api/test-site-configs.js
@@ -9,7 +9,7 @@ function getClient() {
   return createClient(url, key);
 }
 
-export default async function handler(req, res) {
+async function handler(req, res) {
   // 设置CORS头
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
@@ -111,3 +111,5 @@ export default async function handler(req, res) {
     });
   }
 }
+
+module.exports = handler;
